8a728de32ebe011867275ce666c228de7a208fea
PeterM012 <110750833+PeterM012@users.noreply.github.com>
2022-10-08 22:21:49 -0700

    add times to multiple cards

--- script.js
+++ script.js
@@ -10,102 +10,35 @@ function getInfo(){
         console.log(data);
         
         for(i=0; i<6; i++) {
-            $('#allDays').find($("#day"+(i+1)+"Temp").text(`${data.list[i].main.temp}°F`));
-            $('#allDays').find($("#day"+(i+1)+"Wind").text(`${data.list[i].wind.speed} speed`));
-            $('#allDays').find($("#day"+(i+1)+"Humidity").text(`${data.list[i].main.humidity}%`));
+            $('#allDays').find($("#day"+(i+1)+"Temp").text(`${data.list[i].main.temp} °F`));
+            $('#allDays').find($("#day"+(i+1)+"Wind").text(`Wind Speed: ${data.list[i].wind.speed}`));
+            $('#allDays').find($("#day"+(i+1)+"Humidity").text(`Humidity: ${data.list[i].main.humidity}%`));
             $('#allDays').find($("#img"+(i+1)).attr("src", "https://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png"));
- 
-            // console.log(data.list[i].dt);
-            // var weatherDate = moment.unix([data.list[i].dt]).format("MMM Do, YYYY");
-            // $('#allDays').find($("#day"+(i+1)+"Date").text(weatherDate));
-        }
+          }
         
-        for(i=0; i<6; i++) {
-            var currentDate = moment.format("MMM Do, YYYY").add((1), "days");
-            console.log(dates);
-            $('#allDays').find($("#day"+(i+1)+"Date").text(dates));
-        }
-            
+        var currentDate = moment().calendar();        
+        $('#allDays').find($("#day1Date").text(currentDate));
 
+        var date2 = moment().add(1, "days").format("MMMM Do YYYY");
+        console.log(date2);
+        $('#allDays').find($("#day2Date").text(date2));
 
+        var date3 = moment().add(2, "days").format("MMMM Do YYYY");
+        console.log(date3);
+        $('#allDays').find($("#day3Date").text(date3));
+
+        var date4 = moment().add(3, "days").format("MMMM Do YYYY");
+        console.log(date4);
+        $('#allDays').find($("#day4Date").text(date4));
+
+        var date5 = moment().add(4, "days").format("MMMM Do YYYY");
+        console.log(date5);
+        $('#allDays').find($("#day5Date").text(date5));
+
+        var date6 = moment().add(5, "days").format("MMMM Do YYYY");
+        console.log(date6);
+        $('#allDays').find($("#day6Date").text(date6));
     });
 
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let weatherApi = {
-//     "apiKey": "06986c508b07c672d03a8bf63ea59996",
-//     fetchWeatherApi: function (city) {
-//         fetch(
-//             "https://https://api.openweathermap.org/data/2.5/weather?q=" 
-//             + city
-//             + "&units=imperial&appid=" 
-//             + this.apiKey
-//         )
-//         .then((response) => response.json())
-//         .then((data) => this.displayWeather(data));
-//     },
-    // displayWeather: function(data) {
-    //     var { name } = data;
-    //     var { dt } = data;
-    //     var { icon } = data.weather[0];
-    //     var { temp, humidity } = data.main;
-    //     var { speed } = data.wind
-    //     var iconUrl = "http://openweathermap.org/img/w/" + icon + ".png";
-    //     var weatherDate = moment.unix(dt).format("MMM Do, YYYY");
-    //     console.log(name, dt, icon, temp, humidity, speed)
-    //     document.querySelector(".currentCity").innerText = name;
-    //     $('.current').find(".currentDate").text(weatherDate);
-    //     $('.current').find('#currentIcon').attr('src', iconUrl);
-    //     $(".current").find($(".currentTemp")).text(`${temp} °F`);        
-    //     document.querySelector(".currentHumidity").innerText = "Humidity: " + humidity + "%";
-    //     document.querySelector(".currentWind").innerText = "Wind Speed: " + speed;
-
-//         day1(data);
-        // $('#card1').find("#date").text(date);
-        // $('.current').find('#currentIcon').attr('src', iconUrl);
-        // $(".current").find($(".currentTemp")).text(`${temp} °F`);        
-        // document.querySelector(".currentHumidity").innerText = "Humidity: " + humidity + "%";
-        // document.querySelector(".currentWind").innerText = "Wind Speed: " + speed;
-//     },
-//     search: function () {
-//         this.fetchWeatherApi(document.querySelector(".form-control").value);
-//     }
-// };
-
-// document.querySelector(".button").addEventListener("click", function() {
-//     weatherApi.search();
-// })
-
-// function day1 (data) {
-//     for ( let i = 0; i < 5; i++){
-//         let dayOne = data[i];
-//         console.log(data[i]);
-        // document.getElementById("card1" + (i+1).find("#date").text(date));
-//         var date = moment.unix(data.dt).format("MMM Do, YYYY");
-//         $('#card1').find("#date").text(date);
-//         $('#card1').find('#icon').attr('src', iconUrl);
-//         $("#card1").find($("#temp")).text(`${temp} °F`);        
-//         document.querySelector("#humidity").innerText = "Humidity: " + humidity + "%";
-//         document.querySelector("#wind").innerText = "Wind Speed: " + speed;
-//     }
-// }
