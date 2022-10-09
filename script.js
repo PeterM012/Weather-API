9cecaece63b541dc90e3bc1eac04689df99b8258
PeterM012 <110750833+PeterM012@users.noreply.github.com>
2022-10-09 14:38:01 -0700

    add local storage

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var cityHistory = $("#city-history");
+var cities =[];
+
 function getInfo(){
     var cityName = document.getElementById("searchInput");
     var displayCity = document.getElementById("showCity");
@@ -7,7 +10,6 @@ function getInfo(){
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName.value}&units=imperial&appid=06986c508b07c672d03a8bf63ea59996`)
     .then(response => response.json())
     .then(data => {
-        console.log(data);
         
         for(i=0; i<6; i++) {
             $('#allDays').find($("#day"+(i+1)+"Temp").text(`${data.list[i].main.temp} °F`));
@@ -20,25 +22,69 @@ function getInfo(){
         $('#allDays').find($("#day1Date").text(currentDate));
 
         var date2 = moment().add(1, "days").format("MMMM Do YYYY");
-        console.log(date2);
         $('#allDays').find($("#day2Date").text(date2));
 
         var date3 = moment().add(2, "days").format("MMMM Do YYYY");
-        console.log(date3);
         $('#allDays').find($("#day3Date").text(date3));
 
         var date4 = moment().add(3, "days").format("MMMM Do YYYY");
-        console.log(date4);
         $('#allDays').find($("#day4Date").text(date4));
 
         var date5 = moment().add(4, "days").format("MMMM Do YYYY");
-        console.log(date5);
         $('#allDays').find($("#day5Date").text(date5));
 
         var date6 = moment().add(5, "days").format("MMMM Do YYYY");
-        console.log(date6);
         $('#allDays').find($("#day6Date").text(date6));
     });
 
+
+
 }
+
+init();
+function init(){
+    var storedCities = JSON.parse(localStorage.getItem("cities"));
+    if (storedCities !== null) {
+        cities = storedCities;
+      }
+    renderCities();
+}
+
+function storeCities(){
+   localStorage.setItem("cities", JSON.stringify(cities));
+   console.log(localStorage);
+ }
+
+
+function renderCities() {
+    cityHistory.empty();
+    for (var i = 0; i < cities.length; i++) {
+      var city = cities[i];
+      var cityList = $("<button>").text(city);
+      cityList.attr("id","listC");
+      cityList.attr("data-city", city);
+      cityList.attr("class", "d-grid gap-1");
+
+      cityHistory.prepend(cityList);
+    }
+    if (!city){
+        return
+    } 
+    else{
+        getInfo(city)
+    };
+
+}
+
+$("#add-city").on("click", function(event){
+    event.preventDefault();
+  var city = $("#searchInput").val().trim();
+  if (city === "") {
+      return;
+  }
+  cities.push(city);
+    storeCities();
+    renderCities();
+  
+});
 
